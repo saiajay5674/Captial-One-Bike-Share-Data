3813b48ae34fd04d701b83e726a3a44f6d712591
Sai Ajayakumar <SaiAjay@Sais-Mac-mini.local>
2018-10-29 07:50:54 -0400

    Average Distance Design

--- JS/results.js
+++ JS/results.js
@@ -11,7 +11,51 @@ Papa.parse(file, {
 
 function analyse(dataSet)
 {
-    console.log(dataSet);
+
+    console.log(Object.entries(dataSet)[0][1][115]);
+
+    var masterData = Object.entries(dataSet)[0][1];
+
+    //var avgDistance = findAverageDistance(masterData);
+
+    //console.log(avgDistance);
+
+
+    
+}
+// Try using Google Maps Api
+
+function findAverageDistance(masterData)
+{
+    var count = 0;
+    var distance = 0;
+
+    for (var i = 0; i < Object.keys(masterData).length; i++)
+    {
+        var currentData = masterData[i];
+
+        var sLat = currentData['Starting Station Latitude'];
+        var sLong = currentData['Starting Station Longitude'];
+        var eLat = currentData['Ending Station Latitude'];
+        var eLong = currentData['Ending Station Longitude'];
+
+        distance = distance + findDistance(sLat,sLong,eLat,eLong);
+        count = count + 1;
+        
+    }
+
+    return distance/count;
+}
+
+function findDistance(sLat, sLong, eLat, eLong)
+{
+    const R = 6371.01;
+
+    var distance = R * Math.acos(Math.sin(sLat)* Math.sin(eLat) + 
+    Math.cos(sLat)* Math.cos(eLat) * Math.cos(sLong - eLong));
+
+    return distance;
+
 }
 
 
